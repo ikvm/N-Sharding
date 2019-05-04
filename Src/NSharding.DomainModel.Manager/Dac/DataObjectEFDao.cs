dbde69be713098467812ae3af70847aa6274a71b
zhouguoqing <zhouguoqing.eric@gmail.com>
2019-05-04 16:28:37 +0800

    Add DataObjectEFDao

---
+++ Src/NSharding.DomainModel.Manager/Dac/DataObjectEFDao.cs
@@ -0,0 +1,49 @@
+﻿using NSharding.Sharding.Database;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NSharding.DomainModel.Manager.Dac
+{
+    class DataObjectEFDao : DbContext, IDataObjectDao
+    {
+        public DbSet<DataObject> DataObjects { get; set; }
+
+        /// <summary>
+        /// 数据对象保存
+        /// </summary>
+        /// <param name="dataObject">数据对象</param>
+        public void SaveDataObject(DataObject dataObject)
+        {
+            DataObjects.Add(dataObject);
+            this.SaveChanges();
+        }
+
+        /// <summary>
+        /// 删除数据对象
+        /// </summary>
+        /// <param name="id">数据对象ID</param>
+        public void DeleteDataObject(string id)
+        {
+            var obj = DataObjects.FirstOrDefault(i => i.ID == id);
+            if (obj != null)
+            {
+                DataObjects.Remove(obj);
+                this.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// 获取数据对象
+        /// </summary>
+        /// <param name="id">数据对象ID</param>
+        /// <returns>数据对象</returns>
+        public DataObject GetDataObject(string id)
+        {
+            return DataObjects.FirstOrDefault(i => i.ID == id);
+        }
+    }
+}
