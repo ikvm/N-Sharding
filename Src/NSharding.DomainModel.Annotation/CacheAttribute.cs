a6ded9a4d71ef5c497d6dc4527e6c018423f33a2
zhouguoqing <zhouguoqing.eric@gmail.com>
2019-05-26 09:08:57 +0800

    Add CacheAttribute

---
+++ Src/NSharding.DomainModel.Annotation/CacheAttribute.cs
@@ -0,0 +1,32 @@
+﻿using NSharding.DomainModel.Spi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSharding.DomainModel.Annotation
+{
+    /// <summary>
+    /// 缓存选项
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    public class CacheAttribute : Attribute
+    {
+        /// <summary>
+        /// 缓存作用域
+        /// </summary>
+        public CacheScope Scope { get; set; }
+
+        /// <summary>
+        /// 缓存过期时间
+        /// </summary>
+        public long ExpiredTime { get; set; }
+
+        public CacheAttribute(CacheScope scope, long expiredTime = 0)
+        {
+            this.Scope = scope;
+            this.ExpiredTime = expiredTime;
+        }
+    }
+}
---
+++ Src/NSharding.Sharding.DomainModel/CacheScope.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSharding.DomainModel.Spi
+{
+    /// <summary>
+    /// 缓存作用域
+    /// </summary>
+    public enum CacheScope
+    {
+        /// <summary>
+        /// 会话级
+        /// </summary>
+        Session,
+
+        /// <summary>
+        /// 进程级
+        /// </summary>
+        Application,
+
+        /// <summary>
+        /// 全局分布式缓存
+        /// </summary>
+        GLobal
+    }
+}
