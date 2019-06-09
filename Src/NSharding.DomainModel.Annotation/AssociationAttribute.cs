eec9a8fcdffa49679af175e63cf7e0dcb32009db
zhouguoqing <zhouguoqing.eric@gmail.com>
2019-06-09 18:09:04 +0800

    Add AssociationAttribute

---
+++ Src/NSharding.DomainModel.Annotation/AssociationAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSharding.DomainModel.Annotation
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = true)]
+    public class AssociationAttribute : Attribute
+    {
+        /// <summary>
+        /// 领域模型ID
+        /// </summary>
+        public Type DomainModel { get; set; }
+
+
+        /// <summary>
+        /// 是否懒加载
+        /// </summary>
+        public bool IsLazyLoad { get; set; }
+
+        public AssociationAttribute(Type domainModel, bool isLazyLoad)
+        {
+            IsLazyLoad = isLazyLoad;
+            DomainModel = domainModel;
+        }
+    }
+}
---
+++ Src/NSharding.DomainModel.Annotation/InnerJoinAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSharding.DomainModel.Annotation
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = true)]
+    public class InnerJoinAttribute : Attribute
+    {
+    }
+}
