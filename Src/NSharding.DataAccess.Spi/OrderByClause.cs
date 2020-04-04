49b3f135262e6470c784ef38b2e73117253ebb36
zhouguoqing <zhouguoqing.eric@gmail.com>
2020-04-04 10:58:58 +0800

    Add OrderByClause

---
+++ Src/NSharding.DataAccess.Spi/OrderByClause.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSharding.DataAccess.Spi
+{
+    /// <summary>
+    /// 排序条件
+    /// </summary>
+    [Serializable]
+    public class OrderByClause
+    {
+        public List<string> OrderByFields { get; set; }
+
+        public OrderByType OrderByType { get; set; }
+
+        public FieldType FieldType { get; set; }
+
+        public OrderByClause(OrderByType orderByType, FieldType fieldType, params string[] fields)
+        {
+            FieldType = fieldType;
+            OrderByType = orderByType;
+            OrderByFields = fields.ToList();
+        }
+    }
+}
