8fd9fcfc846fdd0b6a2bc00547c974aae29f3151
zhouguoqing <zhouguoqing.eric@gmail.com>
2020-04-05 18:17:39 +0800

    Add RelationalOperator and RelationalOperatorUtis

---
+++ Src/NSharding.DataAccess.Spi/RelationalOperator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSharding.DataAccess.Spi
+{
+    /// <summary>
+    /// 关系运算符
+    /// </summary>
+    public enum RelationalOperator
+    {
+        /// <summary>
+        /// =
+        /// </summary>
+        Equal,
+
+        /// <summary>
+        /// <>
+        /// </summary>
+        NotEqual,
+
+        /// <summary>
+        /// <
+        /// </summary>
+        LessThan,
+
+        /// <summary>
+        /// >
+        /// </summary>
+        GreaterThan,
+
+        /// <summary>
+        /// >=
+        /// </summary>
+        GreaterEqualThan,
+
+        /// <summary>
+        /// <=
+        /// </summary>
+        LessEqualThan,
+
+        /// <summary>
+        /// Like
+        /// </summary>
+        LIKE,
+
+        /// <summary>
+        /// EndLike
+        /// </summary>
+        EndLike,
+
+        /// <summary>
+        /// StartLike
+        /// </summary>
+        StartLike,
+
+        /// <summary>
+        /// In
+        /// </summary>
+        IN,
+
+        /// <summary>
+        /// Between
+        /// </summary>
+        BETWEEN,
+
+        /// <summary>
+        /// Exists
+        /// </summary>
+        Exists
+    }
+}
---
+++ Src/NSharding.DataAccess.Spi/RelationalOperatorUtis.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSharding.DataAccess.Spi
+{
+    public class RelationalOperatorUtis
+    {
+        public static KeyValuePair<string, bool> ConvertToString(RelationalOperator relationalOperator)
+        {
+            switch (relationalOperator)
+            {
+                case RelationalOperator.BETWEEN:
+                    return new KeyValuePair<string, bool>("between {0} and {1}", false);
+                case RelationalOperator.Equal:
+                    return new KeyValuePair<string, bool>("=", true);
+                case RelationalOperator.Exists:
+                    return new KeyValuePair<string, bool>("Exists({0})", false);
+                case RelationalOperator.GreaterEqualThan:
+                    return new KeyValuePair<string, bool>(">=", true);
+                case RelationalOperator.GreaterThan:
+                    return new KeyValuePair<string, bool>(">", true);
+                case RelationalOperator.IN:
+                    return new KeyValuePair<string, bool>("in({0})", false);
+                case RelationalOperator.LessEqualThan:
+                    return new KeyValuePair<string, bool>("<=", true);
+                case RelationalOperator.LessThan:
+                    return new KeyValuePair<string, bool>("<", true);
+                case RelationalOperator.LIKE:
+                    return new KeyValuePair<string, bool>("like '%{0}%'", false);
+                case RelationalOperator.NotEqual:
+                    return new KeyValuePair<string, bool>("<>", true);
+                case RelationalOperator.StartLike:
+                    return new KeyValuePair<string, bool>("like '{0}%'", false);
+                case RelationalOperator.EndLike:
+                    return new KeyValuePair<string, bool>("like '%{0}'", false);
+                default:
+                    return new KeyValuePair<string, bool>("", true);
+            }
+        }
+    }
+}
