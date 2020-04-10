3d15c5b2498547ef7514bdb5fad5d9f8d9394e36
zhouguoqing <zhouguoqing.eric@gmail.com>
2020-04-10 21:46:52 +0800

    Add FilterFieldValue

---
+++ Src/NSharding.DataAccess.Spi/FilterFieldValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSharding.DataAccess.Spi
+{
+    public class FilterFieldValue
+    {
+        public FilterFieldValue()
+        {
+
+        }
+
+        public FilterFieldValue(string fieldValue, FieldValueType fieldValueType, bool isNull = false)
+        {
+            this.FiledValue = fieldValue;
+            this.ValueType = fieldValueType;
+            IsNull = isNull;
+        }
+
+        public FieldValueType ValueType { get; set; }
+
+        public string FiledValue { get; set; }
+
+        public bool IsNull { get; set; }
+    }
+}
