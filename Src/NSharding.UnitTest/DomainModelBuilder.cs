30fddbf7132e58616657701c231527495f0aaa7c
zhouguoqing <zhouguoqing.eric@gmail.com>
2020-05-04 21:33:35 +0800

    Add DomainModelBuilder

--- Src/NSharding.UnitTest/DomainModelBuilder.cs
+++ Src/NSharding.UnitTest/DomainModelBuilder.cs
@@ -1,4 +1,5 @@
-﻿using NSharding.Sharding.Database;
+﻿using NSharding.DomainModel.Spi;
+using NSharding.Sharding.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +23,7 @@ namespace NSharding.UnitTest
                 Name = "销售订单",
                 DataSourceName = "SD",
                 IsTableSharding = false,
-                IsView = false,              
+                IsView = false,
                 Version = 1
             };
 
@@ -31,12 +32,9 @@ namespace NSharding.UnitTest
             dataObject.Columns.Add(new DataColumn() { ID = "SalesOrder_ID", ColumnName = "ID", DisplayName = "ID", DataObjectID = dataObject.ID, DataType = new DataType() { ID = "varchar" }, IsNullable = false, IsPkColumn = true, Length = 36 });
             dataObject.Columns.Add(new DataColumn() { ID = "SalesOrder_Code", ColumnName = "Code", DisplayName = "Code", DataObjectID = dataObject.ID, DataType = new DataType() { ID = "varchar" }, IsNullable = false, IsPkColumn = false, Length = 36 });
             dataObject.Columns.Add(new DataColumn() { ID = "SalesOrder_Description", ColumnName = "Description", DisplayName = "Description", DataObjectID = dataObject.ID, DataType = new DataType() { ID = "varchar" }, IsNullable = true, IsPkColumn = false, Length = 36 });
-           
             dataObject.Columns.Add(new DataColumn() { ID = "SalesOrder_Price", ColumnName = "Price", DisplayName = "Price", DataObjectID = dataObject.ID, DataType = new DataType() { ID = "decimal" }, IsNullable = true, IsPkColumn = false, Length = 9, Precision = 2 });
             dataObject.Columns.Add(new DataColumn() { ID = "SalesOrder_Tax", ColumnName = "Tax", DisplayName = "Tax", DataObjectID = dataObject.ID, DataType = new DataType() { ID = "decimal" }, IsNullable = true, IsPkColumn = false, Length = 9, Precision = 2 });
-           
             dataObject.Columns.Add(new DataColumn() { ID = "SalesOrder_Customer", ColumnName = "Customer", DisplayName = "Customer", DataObjectID = dataObject.ID, DataType = new DataType() { ID = "varchar" }, IsNullable = true, IsPkColumn = false, Length = 36 });
-            
             dataObject.Columns.Add(new DataColumn() { ID = "SalesOrder_CreateTime", ColumnName = "CreateTime", DisplayName = "CreateTime", DataObjectID = dataObject.ID, DataType = new DataType() { ID = "datetime" }, IsNullable = true, IsPkColumn = false, Length = 8 });
             dataObject.Columns.Add(new DataColumn() { ID = "SalesOrder_AdjustReason", ColumnName = "AdjustReason", DisplayName = "AdjustReason", DataObjectID = dataObject.ID, DataType = new DataType() { ID = "varchar" }, IsNullable = true, IsPkColumn = false, Length = 128 });
             dataObject.Columns.Add(new DataColumn() { ID = "SalesOrder_AccountingTime", ColumnName = "AccountingTime", DisplayName = "AccountingTime", DataObjectID = dataObject.ID, DataType = new DataType() { ID = "datetime" }, IsNullable = true, IsPkColumn = false, Length = 8 });
@@ -44,6 +42,67 @@ namespace NSharding.UnitTest
             #endregion
 
             return dataObject;
+        }
+
+        public static DomainModel.Spi.DomainModel CreateDomainModel()
+        {
+            var domainModel = new DomainModel.Spi.DomainModel()
+            {
+                ID = "DM_SalesOrder",
+                Name = "DM_SalesOrder",
+                CacheStrategy = "1",
+                IsCache = true,
+                IsLogicDelete = false,
+                Version = 1
+            };
+
+            var modelObject = new DomainObject()
+            {
+                ID = "SalesOrder",
+                Name = "SalesOrder",
+                DataObjectID = "SalesOrders",
+                ClazzReflectType = "NSharding.UnitTest.SalesOrders,NSharding.UnitTest",
+                IsRootObject = true,
+                DomainModel = domainModel,
+                ParentObject = null
+            };
+
+            modelObject.Elements.Add(
+                new DomainObjectElement()
+                {
+                    ID = "SalesOrders_ID",
+                    Alias = "ID",
+                    Name = "ID",
+                    DataColumnID = "ID",
+                    IsAllowNull = false,
+                    ElementType = ElementType.Normal,
+                    DataType = ElementDataType.String,
+                    DomainObjectID = modelObject.ID,
+                    Length = 36,
+                    PropertyName = "ID",
+                    PropertyType = "System.String",
+                    IsForQuery = true
+                }
+                );
+            modelObject.Elements.Add(
+              new DomainObjectElement()
+              {
+                  ID = "SalesOrders_Code",
+                  Alias = "Code",
+                  Name = "Code",
+                  DataColumnID = "Code",
+                  IsAllowNull = false,
+                  ElementType = ElementType.Normal,
+                  DataType = ElementDataType.String,
+                  DomainObjectID = modelObject.ID,
+                  Length = 36,
+                  PropertyName = "ID",
+                  PropertyType = "System.String",
+                  IsForQuery = true
+              }
+              );
+
+            return domainModel;
         }
     }
 }
